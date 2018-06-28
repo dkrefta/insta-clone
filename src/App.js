64bfe4e817c8da69d53e68f08d3679a7e65de710
Diego Krefta <diego.krefta@gmail.com>
2018-06-28 12:48:58 -0300

    Post changes and Css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
-
 class App extends Component {
   render() {
     return (
-      <Header/>
+      <Header />
     );
   }
 }
-
-export default App;
+export default App
